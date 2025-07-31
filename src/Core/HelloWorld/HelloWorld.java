7f754ead10d214770fb9f2a4f1763f2715431a59
thehappycode <thehappycode@hotmail.com>
2025-08-01 00:00:44 +0700

    #core - add HelloWorld

---
+++ src/Core/HelloWorld/HelloWorld.java
@@ -0,0 +1,9 @@
+// Để compiler sử dụng lệnh javac HelloWorld.java -> ByteCode (*.class)
+// Để JVM chứa interpreter sử dụng lệnh java HelloWorld (không thêm ./ hoặc đuôi file)
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
